9c2049c80698d4cff7d67c69aed7f98dd35d5e4b
Kirill Svidunovich <Altair21817@gmail.com>
2022-12-27 21:04:37 +0300

    Add constants

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -16,17 +16,35 @@ from posts.models import Post
 ONE_MORE_PAGE_POST_COUNT: int = 1
 DIFFERENT_GROUPS_COUNT: int = 2
 
+SLUG = 'test_slug'
+USERNAME_1 = 'Author_1'
+USERNAME_2 = 'Author_2'
+
+INDEX_URL = reverse('posts:index')
+POST_CREATE_URL = reverse('posts:post_create')
+FOLLOW_INDEX_URL = reverse('posts:follow_index')
+GROUP_LIST_URL = reverse('posts:group_list', kwargs={'group': SLUG})
+PROFILE_USER_1_URL = reverse('posts:profile', kwargs={'username': USERNAME_1})
+FOLLOW_USER_2_URL = reverse(
+    'posts:profile_follow',
+    kwargs={'username': USERNAME_2}
+)
+UNFOLLOW_USER_2_URL = reverse(
+    'posts:profile_unfollow',
+    kwargs={'username': USERNAME_2}
+)
+
 
 class PostsViewsTests(TestCase):
     """Тестирование views приложения 'posts'"""
     @classmethod
     def setUpClass(cls) -> None:
         super().setUpClass()
-        cls.USER_1 = User.objects.create(username='Author_1')
-        cls.USER_2 = User.objects.create(username='Author_2')
+        cls.USER_1 = User.objects.create(username=USERNAME_1)
+        cls.USER_2 = User.objects.create(username=USERNAME_2)
         cls.GROUP = Group.objects.create(
             description='Тестовое описание группы',
-            slug='test_slug',
+            slug=SLUG,
             title='Тестовое наименование группы',
         )
         cls.end_rage_bulk_create = (
@@ -62,6 +80,22 @@ class PostsViewsTests(TestCase):
             text='Классный тестовый текст! И комментарий!!',
         )
         cls.COMMENT_ID = Comment.objects.latest('id').id
+        cls.POST_DETAIL_URL = reverse(
+            'posts:post_detail',
+            kwargs={'post_id': PostsViewsTests.POST_ID}
+        )
+        cls.POST_EDIT_URL = reverse(
+            'posts:post_edit',
+            kwargs={'post_id': PostsViewsTests.POST_ID}
+        )
+        cls.ADD_COMMENT_URL = reverse(
+            'posts:add_comment',
+            kwargs={'post_id': PostsViewsTests.POST_ID}
+        )
+        cls.DELETE_COMMENT_URL = reverse(
+            'posts:delete_comment',
+            kwargs={'comment_id': PostsViewsTests.COMMENT_ID}
+        )
 
     def setUp(self):
         cache.clear()
@@ -69,76 +103,36 @@ class PostsViewsTests(TestCase):
     def test_url_use_templates(self) -> None:
         """Тест шаблонов для view функций"""
         test_dict_authorized: dict[str: str] = {
-            reverse('posts:index'): 'includes/posts/index.html',
-            reverse('posts:post_create'): 'includes/posts/create_post.html',
-            reverse('posts:follow_index'): 'includes/posts/follow.html',
-            reverse(
-                'posts:group_list',
-                kwargs={'group': PostsViewsTests.SLUG}
-            ): 'includes/posts/group_list.html',
-            reverse(
-                'posts:post_detail',
-                kwargs={'post_id': PostsViewsTests.POST_ID}
-            ): 'includes/posts/post_detail.html',
-            reverse(
-                'posts:post_edit',
-                kwargs={'post_id': PostsViewsTests.POST_ID}
-            ): 'includes/posts/create_post.html',
-            reverse(
-                'posts:profile',
-                kwargs={'username': PostsViewsTests.USER_1}
-            ): 'includes/posts/profile.html',
-            reverse(
-                'posts:profile_follow',
-                kwargs={'username': PostsViewsTests.USER_2}
-            ): 'includes/posts/profile.html',
-            reverse(
-                'posts:profile_unfollow',
-                kwargs={'username': PostsViewsTests.USER_2}
-            ): 'includes/posts/profile.html',
+            INDEX_URL: 'includes/posts/index.html',
+            POST_CREATE_URL: 'includes/posts/create_post.html',
+            FOLLOW_INDEX_URL: 'includes/posts/follow.html',
+            GROUP_LIST_URL: 'includes/posts/group_list.html',
+            PostsViewsTests.POST_DETAIL_URL: 'includes/posts/post_detail.html',
+            PostsViewsTests.POST_EDIT_URL: 'includes/posts/create_post.html',
+            PROFILE_USER_1_URL: 'includes/posts/profile.html',
+            FOLLOW_USER_2_URL: 'includes/posts/profile.html',
+            UNFOLLOW_USER_2_URL: 'includes/posts/profile.html',
         }
         test_dict_authorized_follow = {
-            reverse(
-                'posts:add_comment',
-                kwargs={'post_id': PostsViewsTests.POST_ID}
-            ): 'includes/posts/post_detail.html',
-            reverse(
-                'posts:delete_comment',
-                kwargs={'comment_id': PostsViewsTests.COMMENT_ID}
-            ): 'includes/posts/post_detail.html',
+            PostsViewsTests.ADD_COMMENT_URL: 'includes/posts/post_detail.html',
+            PostsViewsTests.DELETE_COMMENT_URL: (
+                'includes/posts/post_detail.html'
+            ),
         }
         test_list_guest: list[reverse] = [
-            reverse(
-                'posts:add_comment',
-                kwargs={'post_id': PostsViewsTests.POST_ID}
-            ),
-            reverse(
-                'posts:delete_comment',
-                kwargs={'comment_id': PostsViewsTests.COMMENT_ID}
-            ),
-            reverse('posts:follow_index'),
-            reverse('posts:post_create'),
-            reverse(
-                'posts:post_edit',
-                kwargs={'post_id': PostsViewsTests.POST_ID}
-            ),
-            reverse(
-                'posts:profile_follow',
-                kwargs={'username': PostsViewsTests.USER_2}
-            ),
-            reverse(
-                'posts:profile_unfollow',
-                kwargs={'username': PostsViewsTests.USER_2}
-            ),
+            PostsViewsTests.ADD_COMMENT_URL,
+            PostsViewsTests.DELETE_COMMENT_URL,
+            FOLLOW_INDEX_URL,
+            POST_CREATE_URL,
+            PostsViewsTests.POST_EDIT_URL,
+            FOLLOW_USER_2_URL,
+            UNFOLLOW_USER_2_URL,
         ]
         test_dict_guest = test_dict_authorized.copy()
         for path in test_list_guest:
             test_dict_guest[path] = ('includes/users/login.html')
         response_2 = PostsViewsTests.AUTHORIZED_CLIENT_2.get(
-            reverse(
-                'posts:delete_comment',
-                kwargs={'comment_id': PostsViewsTests.COMMENT_ID}
-            ),
+            PostsViewsTests.DELETE_COMMENT_URL,
             follow=True
         )
         self.assertTemplateUsed(response_2, 'includes/posts/post_detail.html')
@@ -165,7 +159,7 @@ class PostsViewsTests(TestCase):
 
     def test_page_posts_create_post(self) -> None:
         """Тест URL 'posts:create_post'"""
-        response = self.AUTHORIZED_CLIENT_1.get(reverse('posts:post_create'))
+        response = self.AUTHORIZED_CLIENT_1.get(POST_CREATE_URL)
         test_dict = {
             'group': models.ModelChoiceField,
             'text': fields.CharField,
@@ -183,10 +177,7 @@ class PostsViewsTests(TestCase):
     def test_page_post_post_detail(self) -> None:
         """Тест URL 'post:post_detail'"""
         response = self.AUTHORIZED_CLIENT_1.get(
-            reverse(
-                'posts:post_detail',
-                kwargs={'post_id': PostsViewsTests.POST_ID}
-            )
+            PostsViewsTests.POST_DETAIL_URL
         )
         self.assertEqual(
             Post.objects.get(id=PostsViewsTests.POST_ID).text,
@@ -198,12 +189,7 @@ class PostsViewsTests(TestCase):
 
     def test_page_post_post_edit(self) -> None:
         """Тест URL 'post:post_edit'"""
-        response = self.AUTHORIZED_CLIENT_1.get(
-            reverse(
-                'posts:post_edit',
-                kwargs={'post_id': PostsViewsTests.POST_ID}
-            )
-        )
+        response = self.AUTHORIZED_CLIENT_1.get(PostsViewsTests.POST_EDIT_URL)
         test_form_dict = {
             'group': models.ModelChoiceField,
             'text': fields.CharField,
@@ -223,12 +209,7 @@ class PostsViewsTests(TestCase):
 
     def test_page_post_profile(self) -> None:
         """Тест URL 'post:profile'"""
-        response = self.AUTHORIZED_CLIENT_1.get(
-            reverse(
-                'posts:profile',
-                kwargs={'username': PostsViewsTests.USER_1}
-            )
-        )
+        response = self.AUTHORIZED_CLIENT_1.get(PROFILE_USER_1_URL)
         response_value = str(response.context['author'])
         self.assertEqual(response_value, str(PostsViewsTests.USER_1))
 
@@ -239,13 +220,9 @@ class PostsViewsTests(TestCase):
             text='Пост для теста подписки',
         )
         follow_post = Post.objects.get(id=PostsViewsTests.POST_ID + 1)
-        response = PostsViewsTests.GUEST_CLIENT.get(
-            reverse('posts:follow_index')
-        )
+        response = PostsViewsTests.GUEST_CLIENT.get(FOLLOW_INDEX_URL)
         self.assertEqual(response.status_code, HTTPStatus.FOUND)
-        response = PostsViewsTests.AUTHORIZED_CLIENT_1.get(
-            reverse('posts:follow_index')
-        )
+        response = PostsViewsTests.AUTHORIZED_CLIENT_1.get(FOLLOW_INDEX_URL)
         self.assertEqual(response.status_code, HTTPStatus.OK)
         subscriptions = {
             PostsViewsTests.GUEST_CLIENT: [PostsViewsTests.USER_1, 0],
@@ -258,9 +235,7 @@ class PostsViewsTests(TestCase):
                     f'/profile/{result[0]}/follow/'
                 )
                 self.assertEqual(Follow.objects.count(), result[1])
-        response = PostsViewsTests.AUTHORIZED_CLIENT_1.get(
-            reverse('posts:follow_index')
-        )
+        response = PostsViewsTests.AUTHORIZED_CLIENT_1.get(FOLLOW_INDEX_URL)
         len_context = len(response.context['page_obj'])
         self.assertEqual(len_context, 1)
         self.assertEqual(
@@ -271,9 +246,7 @@ class PostsViewsTests(TestCase):
             f'/profile/{PostsViewsTests.USER_2}/unfollow/'
         )
         self.assertEqual(Follow.objects.count(), 0)
-        response = PostsViewsTests.AUTHORIZED_CLIENT_1.get(
-            reverse('posts:follow_index')
-        )
+        response = PostsViewsTests.AUTHORIZED_CLIENT_1.get(FOLLOW_INDEX_URL)
         self.assertEqual(response.status_code, HTTPStatus.OK)
         follow_post.delete()
 
@@ -288,17 +261,10 @@ class PostsViewsTests(TestCase):
             2: PAGE_POST_COUNT,
             3: (ONE_MORE_PAGE_POST_COUNT * DIFFERENT_GROUPS_COUNT),
         }
-        group_list_reverse = reverse(
-            'posts:group_list',
-            kwargs={'group': PostsViewsTests.SLUG}
-        )
         test_dict_reverse = {
-            reverse('posts:index'): test_dict_count_3,
-            reverse(
-                'posts:profile',
-                kwargs={'username': str(PostsViewsTests.USER_1)}
-            ): test_dict_count_3,
-            group_list_reverse: test_dict_count_2,
+            INDEX_URL: test_dict_count_3,
+            PROFILE_USER_1_URL: test_dict_count_3,
+            GROUP_LIST_URL: test_dict_count_2,
         }
         for path, count in test_dict_reverse.items():
             for field, expected in count.items():
@@ -320,7 +286,7 @@ class PostsViewsTests(TestCase):
                     self.assertEqual(response_value, expected)
                     response_value = list(response.context['page_obj'])
                     cache.clear()
-                    if group_list_reverse == path:
+                    if path == GROUP_LIST_URL:
                         expected = list(
                             Post.objects.filter(
                                 group=PostsViewsTests.GROUP
@@ -334,30 +300,22 @@ class PostsViewsTests(TestCase):
 
     def test_cache_for_index(self) -> None:
         """Тест cache для страницы 'posts:index'"""
-        response = PostsViewsTests.AUTHORIZED_CLIENT_1.get(
-            reverse('posts:index')
-        )
+        response = PostsViewsTests.AUTHORIZED_CLIENT_1.get(INDEX_URL)
         old_content = response.content
         new_post_data = {'text': 'Пост для тестирования cache'}
         PostsViewsTests.AUTHORIZED_CLIENT_1.post(
-            reverse('posts:post_create'),
+            POST_CREATE_URL,
             data=new_post_data,
         )
         new_post_id = Post.objects.latest('id').id
         self.assertEqual(new_post_id, PostsViewsTests.POST_ID + 1)
-        response = PostsViewsTests.AUTHORIZED_CLIENT_1.get(
-            reverse('posts:index')
-        )
+        response = PostsViewsTests.AUTHORIZED_CLIENT_1.get(INDEX_URL)
         cached_content = response.content
         Post.objects.filter(id=new_post_id).delete()
-        response = PostsViewsTests.AUTHORIZED_CLIENT_1.get(
-            reverse('posts:index')
-        )
+        response = PostsViewsTests.AUTHORIZED_CLIENT_1.get(INDEX_URL)
         new_content = response.content
         self.assertEqual(new_content, cached_content)
         cache.clear()
-        response = PostsViewsTests.AUTHORIZED_CLIENT_1.get(
-            reverse('posts:index')
-        )
+        response = PostsViewsTests.AUTHORIZED_CLIENT_1.get(INDEX_URL)
         uncached_content = response.content
         self.assertEqual(uncached_content, old_content)
