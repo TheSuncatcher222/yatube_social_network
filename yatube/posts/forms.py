f267ce13204e32ba54a4eca0c679875b36747ba5
Kirill Svidunovich <Altair21817@gmail.com>
2022-12-27 15:17:33 +0300

    Add widget (text rows number) to field CommentForm.fields[text]

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -17,3 +17,7 @@ class CommentForm(ModelForm):
     class Meta:
         model = Comment
         fields = ('text',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['text'].widget.attrs.update(rows='1')
